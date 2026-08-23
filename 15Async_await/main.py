1ec6a4f9d776de33773215645c097caf0b9558da
Gaurav <graj75657@gmail.com>
2026-08-23 15:35:54 +0530

    Async Await

---
+++ 15Async_await/main.py
@@ -0,0 +1,14 @@
+import time
+import asyncio
+from fastapi import FastAPI
+app = FastAPI()
+
+
+async  def task():
+    await asyncio.sleep(3)
+    return  "Done"
+
+@app.get("/")
+async def home():
+    await asyncio.sleep(3)
+    return {"message":"Async await in FastAPI"}
